fd1c7bff7a74b2f8c0b7efbd179e905781b1c467
Ilakkiya182005 <115527324+Ilakkiya182005@users.noreply.github.com>
2024-07-21 09:11:57 +0530

    first commit

---
+++ list.js
@@ -0,0 +1,41 @@
+const InputBox = document.getElementById("input-box");
+const ListContainer = document.getElementById("list-container")
+
+function addTask(){
+    if(InputBox.value==''){
+        alert("you must return something")
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML = InputBox.value;
+        ListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+
+
+    }
+    InputBox.value="";
+    SaveData();
+
+}
+ListContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        SaveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        SaveData();
+    }
+},false);
+
+function SaveData(){
+    localStorage.setItem("data",ListContainer.innerHTML);
+
+}
+
+function ShowData(){
+    ListContainer.innerHTML=localStorage.getItem("data");
+}
+ShowData();
